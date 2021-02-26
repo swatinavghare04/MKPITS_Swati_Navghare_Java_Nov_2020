f6254201d46369d1f38e9ffd7381bfb41574aaa8
HARSHAD <navghareswati70@gmail.com>
2021-02-26 13:07:44 +0530

    core java practice folder updated

---
+++ Core_Java_Practice/NumberPattern.java
@@ -0,0 +1,16 @@
+// WAP for numeric pattern - Simple number program.
+
+public class NumberPattern {
+
+  public static void main(String[] args){
+
+    for(int i=1;i<=5;i++){
+
+     for( int j=1;j<=i;j++){
+
+        System.out.print(j);
+     }
+        System.out.println();
+    }
+  }
+}
