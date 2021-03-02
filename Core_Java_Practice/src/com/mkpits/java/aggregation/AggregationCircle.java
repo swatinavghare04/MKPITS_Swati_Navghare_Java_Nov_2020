0c340ff3e85b68790285be5344a75b00d7a2212d
HARSHAD <navghareswati70@gmail.com>
2021-03-02 11:44:02 +0530

    core java practice folder updated

--- Core_Java_Practice/src/com/mkpits/java/aggregation/AggregationCircle.java
+++ Core_Java_Practice/src/com/mkpits/java/aggregation/AggregationCircle.java
@@ -17,8 +17,8 @@ class Operation{
        return pi*rsquare;  
      }  
                   
-     public static void main(String args[]){  
-       Circle c=new Circle();  
+     public static void main(String args[]){
+         AggregationCircle c=new AggregationCircle();
        double result=c.area(5);  
        System.out.println(result);  
      }  
